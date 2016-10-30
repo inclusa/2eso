025cc2407034942d196e12c063e5a076ca394c04
Alfons Rovira <inclusa@gmail.com>
2016-10-30 17:42:50 +0100

    Creada recopil·lació de música en pdf

--- 05_mu/md/generator_a5.sh
+++ 05_mu/md/generator_a5.sh
@@ -2,5 +2,6 @@
 
 # pandoc --template mytemplate.tex --toc -V fontsize=12pt -o output.pdf input1.md input2.md 
 
-pandoc --template template_a5_h.tex --toc -V fontsize=12pt -o tablet.pdf  01_ud01.md
+pandoc --template template_a5_h.tex --toc -V fontsize=12pt -o tablet.pdf  01_ud01.md 02_ud02.md 03_ud03.md
+
 
