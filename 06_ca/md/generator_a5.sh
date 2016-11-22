1fc6bb21f0d9a58b0c6820b6838defdda63d59c9
Alfons Rovira <inclusa@gmail.com>
2016-11-22 23:09:39 +0100

    Depuració ortogràfica Música PDF 2ESO

--- 06_ca/md/generator_a5.sh
+++ 06_ca/md/generator_a5.sh
@@ -2,5 +2,5 @@
 
 # pandoc --template mytemplate.tex --toc -V fontsize=12pt -o output.pdf input1.md input2.md 
 
-pandoc --template template_a5_h.tex --toc -V fontsize=12pt -o tablet.pdf  0_textos.md
+pandoc --template template_a5_h.tex --toc -V fontsize=12pt  -o tablet.pdf  0_textos.md 01_ud01.md 02_ud02.md
 
