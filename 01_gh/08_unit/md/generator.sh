ec09e50aa77a28a977aa468a8d0dbacacc0fed80
Alfons Rovira <inclusa@gmail.com>
2017-06-04 15:19:41 +0200

    UD 8 GEH - mapa mental

--- 01_gh/08_unit/md/generator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# pandoc --template mytemplate.tex --toc -V fontsize=12pt -o output.pdf input1.md input2.md 
-
-pandoc --template template.tex --toc -V fontsize=12pt -o 11_tablet.pdf tot.md
